e4a2f0fa398afc9d766c8370ad0429b0ed156855
talabidele <talabibamidele7@gmail.com>
2022-12-16 21:43:24 +0100

    Code updated

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Navbar from './components/Nav/Navbar';
-import working from './images/illustration-working.svg'
-import UrlShortner from './components/urlShortner/urlShortner';
-import Advance from './pages/Advance';
-import data from '../src/data'
-import List from './components/List/list';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Nav/Navbar";
+import working from "./images/illustration-working.svg";
+import UrlShortner from "./components/urlShortner/urlShortner";
+import Advance from "./pages/Advance";
+import data from "../src/data";
+import List from "./components/List/list";
 
 function App() {
-     const[stat,setStat] = useState(data);
+  const [stat, setStat] = useState(data);
 
   return (
-          <div className='container'>
-            <section className='page1'>
-            <Navbar/>
-            <div className='heros'>
-             <div className='body-info'>
-                <div className='info-text'>
-                 <h1 className='info1'>More than just shorter links.</h1>
-               <h4> Build your brand's recognition and get detailed insights on how your links are performing</h4>
-                  <button className='getstarted'>Get Started</button>             
-                 </div>
-                 <img src={working} className='working'/>
-             </div>
-             </div>
-            </section>
-             <UrlShortner/>
-             <section>
-               <Advance/>
-             </section>
-             <section>
-              <div  className='boost'>
-                <div className='boost-info'>
-                <h2>Boost your links today</h2>
-                <button className='boost-btn'>Get Started</button>
-                </div>
-                
-              </div>
-             </section>
-             
+    <div className="container">
+      <section className="page1">
+        <Navbar />
+        <div className="heros">
+          <div className="body-info">
+            <div className="info-text">
+              <h1 className="info1">More than just shorter links.</h1>
+              <h4>
+                {" "}
+                Build your brand's recognition and get detailed insights on how
+                your links are performing
+              </h4>
+              <button className="getstarted">Get Started</button>
+            </div>
+            <img src={working} className="working" />
           </div>
+        </div>
+      </section>
+      <section>
+        {/* <UrlShortner /> */}
+        <Advance />
+      </section>
+      <section>
+        <div className="boost">
+          <div className="boost-info">
+            <h2>Boost your links today</h2>
+            <button className="boost-btn">Get Started</button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/components/urlShortner/urlShortner.jsx
+++ src/components/urlShortner/urlShortner.jsx
@@ -1,78 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import '../urlShortner/urlshortener.css';
-
-
+import React, { useEffect, useState } from "react";
+import "../urlShortner/urlshortener.css";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const UrlShortner = () => {
-  const getData = () =>{
-    const list = localStorage.getItem('urlData')
-  }
- 
-   const[userUrl,setUserurl] = useState('');
-   const[urlData,setUrlData] = useState({})
-   const[buttonCopy,setButtonCopy] = useState("Copy");
-   const[active,setActive] =useState(false);
+  // const getData = () => {
+  //   const list = localStorage.getItem("urlData");
+  // };
 
+  const [userUrl, setUserurl] = useState("");
+  const [urlData, setUrlData] = useLocalStorage("urlData", []);
+  const [buttonCopy, setButtonCopy] = useState("Copy");
+  const [active, setActive] = useState(false);
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        if(!userUrl){
-          alert('Input is empty');
-        }else{
-          const shortenLink = () =>{
-            fetch(`https://api.shrtco.de/v2/shorten?url=${userUrl}`)
-          .then(response =>{
-          if(response.ok){
-              return response.json()
-          }
-          throw response
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userUrl === "") {
+      alert("Input is empty");
+    } else {
+      const shortenLink = () => {
+        fetch(`https://api.shrtco.de/v2/shorten?url=${userUrl}`)
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw response;
           })
-          .then(data =>{
-            setUrlData(data.result)
-            setUserurl('')
+          .then((data) => {
+            console.log(data.result);
+            setUrlData([
+              ...urlData,
+              {
+                id: data.result.code,
+                url: data.result.original_link,
+                link: data.result.short_link,
+              },
+            ]);
+            // setUrlData(data.result);
+            setUserurl("");
             // setUserurl("");
-            setActive(true)
-          console.log(urlData);
-          }).catch(error =>{
-          console.error("Error Fetching :", error)
+            setActive(true);
+            console.log(urlData);
           })
+          .catch((error) => {
+            console.error("Error Fetching :", error);
+          });
+      };
+      shortenLink();
+    }
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(urlData.full_short_link);
+    setButtonCopy("Copied");
+  };
+  // useEffect(() => {
+  //   localStorage.setItem(urlData, JSON.stringify("urlData"));
+  // }, [urlData]);
 
-      }
-            shortenLink();
-        }
-        }
-        const handleCopy = () =>{
-          navigator.clipboard.writeText(urlData.full_short_link);
-          setButtonCopy("Copied")
-        }
-        useEffect(() =>{
-          localStorage.setItem(urlData, JSON.stringify("urlData"))
+  // getData = () => {
+  //   const list = localStorage.getItem("urlDat");
+  // };
 
-        },[urlData]);
-
-        getData =>() =>{
-          const list = localStorage.getItem("urlDat")
-        }
-        
   return (
     <div>
-    <div className='formControl'>
-        <form onSubmit={handleSubmit} className=''>
-            <input placeholder='shorten your link here...' onChange={(e) => setUserurl(e.target.value)}/>
-            <button>Shorten It</button>
-        </form>      
+      <div className="formControl">
+        <form onSubmit={handleSubmit} className="">
+          <input
+            placeholder="shorten your link here..."
+            onChange={(e) => setUserurl(e.target.value)}
+            value={userUrl}
+          />
+          <button>Shorten It</button>
+        </form>
+      </div>
+      {active &&
+        urlData.map((e) => (
+          <div className="result">
+            <h4 style={{ color: "black" }}>{e.url}</h4>
+            <div className="list">
+              <h4 className="short-text">{e.link}</h4>
+              <button onClick={handleCopy}> {buttonCopy}</button>
+            </div>
+          </div>
+        ))}
     </div>
-    {active && 
-    <div className='result'>
-        <h4 style={{color:'black'}}>{urlData.original_link}</h4>
-        <div className='list'>
-        <h4 className='short-text'>{urlData.full_short_link}</h4>
-        <button onClick={handleCopy}> {buttonCopy}</button>
-        </div>
-    </div>}
-
-    </div>
-  )
-}
+  );
+};
 
 export default UrlShortner;
